45d58c38d4931dd02846dfdaa3e02b91f5003478
Djordje <dd222im@student.lnu.se>
2023-09-25 23:08:18 +0200

    Some small edits in main file problem 3

--- src/problem3/Main.java
+++ src/problem3/Main.java
@@ -12,7 +12,7 @@ public class Main {
     public static void main(String[] args) {
 
 
-
+        System.out.println("Provide the directory to list the files and form a tree: ");
         Scanner scanner = new Scanner(System.in);
         String path = scanner.nextLine();
 
@@ -20,9 +20,12 @@ public class Main {
 
         tree.walkThroughTree();
 
+        //tree.addChild(); TODO: adding a child will list all the directories where
+        //                      a child can be added and it will require input for a child name.
 
-        TreeNode tn = tree.getNodeByName("2");
-        System.out.println(tn.toString());
+
+        //TreeNode tn = tree.getNodeByName(" "); TODO: here you can provide a name of the node to get all the details
+        //System.out.println(tn.toString());
 
 
     }
