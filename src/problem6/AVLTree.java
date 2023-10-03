3ddc033c8c2d6092c1d9bef15ef505b13a81f464
dimit <dimitrovdjordje@gmail.com>
2023-10-03 16:47:37 +0200

    problem6 initial commit.

--- src/problem6/AVLTree.java
+++ src/problem6/AVLTree.java
@@ -6,7 +6,7 @@ public class AVLTree<AnyType extends Comparable<? super AnyType>> extends Binary
 
 
     public void add(AnyType x) {
-        root = add(x, root);
+        add(x, root);
     }
 
     private BinaryNode<AnyType> add(AnyType x, BinaryNode<AnyType> t) {
