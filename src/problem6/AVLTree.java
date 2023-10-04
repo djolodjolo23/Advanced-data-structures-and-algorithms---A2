ed5de30082ff8068feb3d8fda7bf9df751697118
Djordje <dd222im@student.lnu.se>
2023-10-04 09:30:28 +0200

    working on avl tree

--- src/problem6/AVLTree.java
+++ src/problem6/AVLTree.java
@@ -5,6 +5,20 @@ import problem4.BinarySearchTree;
 public class AVLTree<AnyType extends Comparable<? super AnyType>> extends BinarySearchTree<AnyType> {
 
 
+    public static class AVLNode<AnyType> {
+        public AnyType element;
+        public BinarySearchTree.BinaryNode<AnyType> left;
+        public BinarySearchTree.BinaryNode<AnyType> right;
+
+        AVLNode(AnyType element) {
+            this(element, null, null);
+        }
+        AVLNode(AnyType theElement, BinarySearchTree.BinaryNode<AnyType> lt, BinarySearchTree.BinaryNode<AnyType> rt){
+            element = theElement; left = lt; right = rt;
+        }
+    }
+
+    public BinarySearchTree.BinaryNode<AnyType> root;
     public void add(AnyType x) {
         add(x, root);
     }
