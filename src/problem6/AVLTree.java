49aa0c469147a95cb23a9474de84fa5037fed2a4
dimit <dimitrovdjordje@gmail.com>
2023-10-04 11:02:22 +0200

    Working on avl tree continued.

--- src/problem6/AVLTree.java
+++ src/problem6/AVLTree.java
@@ -6,24 +6,96 @@ public class AVLTree<AnyType extends Comparable<? super AnyType>> extends Binary
 
 
     public static class AVLNode<AnyType> {
-        public AnyType element;
-        public BinarySearchTree.BinaryNode<AnyType> left;
-        public BinarySearchTree.BinaryNode<AnyType> right;
+        AnyType element;
+        AVLNode<AnyType> left;
+        AVLNode<AnyType> right;
+
+        int height;
 
         AVLNode(AnyType element) {
             this(element, null, null);
         }
-        AVLNode(AnyType theElement, BinarySearchTree.BinaryNode<AnyType> lt, BinarySearchTree.BinaryNode<AnyType> rt){
-            element = theElement; left = lt; right = rt;
+        AVLNode(AnyType theElement, AVLNode<AnyType> lt, AVLNode<AnyType> rt){
+            element = theElement; left = lt; right = rt; height = 0;
         }
     }
 
-    public BinarySearchTree.BinaryNode<AnyType> root;
-    public void add(AnyType x) {
-        add(x, root);
+    public AVLTree() {
+        this.root = null;
     }
 
-    private BinaryNode<AnyType> add(AnyType x, BinaryNode<AnyType> t) {
+    public AVLNode<AnyType> root;
+    public void add(AnyType x) {
+        root = add(x, root);
+    }
+
+    private AVLNode<AnyType> add(AnyType x, AVLNode<AnyType> t) {
+        if (t == null) {
+            return new AVLNode<>(x, null, null); // no more nodes to traverse
+        }
+        int compareResult = x.compareTo(t.element);
+        if (compareResult < 0) {
+            t.left = add(x, t.left);
+        } else if (compareResult > 0) {
+            t.right = add(x, t.right);
+        }
+        return balance(t);
+    }
+
+    private AVLNode<AnyType> balance(AVLNode<AnyType> t) {
+        if (t == null) {
+            return null;
+        }
+        if (height(t.left) - height(t.right) > 1) {
+            if (height(t.left.left) >=height(t.left.right)) {
+                t = rotateLeft(t);
+            } else {
+                t = doubleLeft(t);
+            }
+        }
+        else if (height(t.right) - height(t.left) > 1) {
+            if (height(t.right.right) >= height(t.right.left)) {
+                t = rotateRight(t); // testing
+            } else {
+                t = doubleRight(t);
+            }
+        }
+        t.height = Math.max(height(t.left), height(t.right)) + 1;
         return t;
+    }
+
+    private int height(AVLNode<AnyType> t) {
+        if (t == null) {
+            return - 1;
+        }
+        return t.height;
+    }
+
+    private AVLNode<AnyType> rotateLeft(AVLNode<AnyType> t2){
+        AVLNode<AnyType> t1 = t2.left;
+        t2.left = t1.right;
+        t1.right = t2;
+        t2.height = Math.max(height(t2.left) , height(t2.right)) + 1;
+        t1.height = Math.max(height(t1.left), t2.height) + 1;
+        return t1;
+    }
+
+    private AVLNode<AnyType> rotateRight(AVLNode<AnyType> t2) {
+        AVLNode<AnyType> t1 = t2.right;
+        t2.right = t1.left;
+        t1.left = t2;
+        t2.height = Math.max(height(t2.left), height(t2.right)) + 1;
+        t1.height = Math.max(height(t1.left), t2.height) + 1;
+        return t1;
+    }
+
+    private AVLNode<AnyType> doubleLeft (AVLNode<AnyType> t) {
+        t.left = rotateRight(t.left);
+        return rotateLeft(t);
+    }
+
+    private AVLNode<AnyType> doubleRight (AVLNode<AnyType> t) {
+        t.left = rotateLeft(t.left);
+        return rotateRight(t);
     }
 }
--- src/problem6/Main.java
+++ src/problem6/Main.java
@@ -1,4 +1,14 @@
 package problem6;
 
 public class Main {
+
+    public static void main(String[] args) {
+
+
+        AVLTree<Integer> avlTree = new AVLTree<>();
+
+        avlTree.add(3);
+        avlTree.add(5);
+        avlTree.add(7);
+    }
 }
